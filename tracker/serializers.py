0ceae88f430a674f078cab0df27876d4b1a41e08
Galina Lukina <lukina.galina11@gmail.com>
2025-02-02 20:56:52 +0400

    change fields duration and periodicity

--- tracker/serializers.py
+++ tracker/serializers.py
@@ -9,6 +9,10 @@ class UsefulHabitSerializer(serializers.ModelSerializer):
         MaxValueValidator(7, message="Нельзя выполнять привычку реже, чем 1 раз в 7 дней"),
         MinValueValidator(1, message="Минимальное значение 1")])
 
+    duration = serializers.IntegerField(validators=[
+        MaxValueValidator(120, message="Максимальное время выполнения привычки 120 секунд"),
+        MinValueValidator(1, message="Минимальное время выполнения 1 секунда")])
+
     def validate(self, data):
         """Проверяет, что выбрано только одно поле варианта поощрения."""
         if data.get('related_habit') and data.get('award'):
